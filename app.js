6eb8ff8f29fc0ec150f87a71be5747d766b5bee9
mrpeepernumber007 <federico.e.naso@gmail.com>
2023-09-12 11:03:14 -0300

    Creates basic files, makes grid of boxes and allows input to determine size of grid

---
+++ app.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('.container')
+const canvasBtn = document.querySelector('.btn')
+
+function boxMaker (size) {
+    for (let i = 0; i < (size); i++) {
+        let line = document.createElement('div')
+        line.classList.add('.line')
+        container.appendChild(line)
+        for (let j = 0; j < size; j++) {
+            let box = document.createElement('div')
+            box.classList.add('box')
+            line.appendChild(box)
+            box.addEventListener('click', () => box.classList.add('hovered-box'))
+        }
+    }
+}
+
+canvasBtn.addEventListener('click', () => {
+    let boxNum = prompt('How many boxes?', 16)
+    let lines = document.querySelectorAll('.line')
+    let boxes = document.querySelectorAll('.box')
+    lines.forEach(line => line.remove())
+    boxes.forEach(box => box.remove())
+    boxMaker(boxNum)
+})
+
+boxMaker(16)
