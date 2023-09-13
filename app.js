d28e55e201ee3db334cb0367e6e51baa4da0667b
mrpeepernumber007 <federico.e.naso@gmail.com>
2023-09-13 09:48:16 -0300

    Test one branch method

--- app.js
+++ app.js
@@ -2,28 +2,18 @@ const container = document.querySelector('.container')
 const canvasBtn = document.querySelector('.btn')
 
 function boxMaker (size) {
-    for (let i = 0; i < (size); i++) {
-        let line = document.createElement('div')
-        line.classList.add('.line')
-        container.appendChild(line)
-        for (let j = 0; j < size; j++) {
-            let box = document.createElement('div')
-            box.classList.add('box')
-            line.appendChild(box)
-            box.addEventListener('click', () => box.classList.add('hovered-box'))
-        }
+    for (let i = 0; i < (size*size); i++) {
+        let box = document.createElement('div')
+        box.classList.add('box')
+        container.appendChild(box)
     }
 }
 
 canvasBtn.addEventListener('click', () => {
     let boxNum = prompt('How many boxes?', 16)
-    let lines = document.querySelectorAll('.line')
     let boxes = document.querySelectorAll('.box')
-    lines.forEach(line => line.remove())
     boxes.forEach(box => box.remove())
     boxMaker(boxNum)
 })
 
-boxMaker(16)
-
-//test comment, please ignore
+boxMaker(5)
