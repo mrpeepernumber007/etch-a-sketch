11759f9d1e3edb181c55aa123ef1c289112fab92
mrpeepernumber007 <federico.e.naso@gmail.com>
2023-09-13 10:18:18 -0300

    Make box size dynamic

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const canvasBtn = document.querySelector('.btn')
 function boxMaker (size) {
     for (let i = 0; i < (size); i++) {
         let line = document.createElement('div')
-        line.classList.add('.line')
+        line.classList.add('line')
         container.appendChild(line)
         for (let j = 0; j < size; j++) {
             let box = document.createElement('div')
             box.classList.add('box')
+            box.style.width = `calc(960px * (1/${size}))`
+            box.style.height = `calc(960px * (1/${size}))`
             line.appendChild(box)
-            box.addEventListener('click', () => box.classList.add('hovered-box'))
+            box.addEventListener('mouseover', () => box.classList.add('hovered-box'))
         }
     }
 }
@@ -19,12 +21,9 @@ canvasBtn.addEventListener('click', () => {
     let boxNum = prompt('How many boxes?', 16)
     let lines = document.querySelectorAll('.line')
     let boxes = document.querySelectorAll('.box')
-    lines.forEach(line => line.remove())
     boxes.forEach(box => box.remove())
+    lines.forEach(line => line.remove())
     boxMaker(boxNum)
 })
 
-boxMaker(16)
-
-
-//nothing to see here
+boxMaker(5)
